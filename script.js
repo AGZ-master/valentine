f6d0d6e9d26c5391fad10f2115bd49862bc7f8a4
AGZ-master <amirghanizadeh209@gmail.com>
2025-02-04 23:57:50 +0330

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const x = Math.random() * (window.innerWidth - noButton.offsetWidth);
         const y = Math.random() * (window.innerHeight - noButton.offsetHeight);
 
+        noButton.style.position = 'absolute';
         noButton.style.left = x + "px";
         noButton.style.top = y + "px";
     }
